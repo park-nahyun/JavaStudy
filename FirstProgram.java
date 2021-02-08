a2420fee60f7eb04deb4adcbef790ce8a09c6419
parknahyun <youbetterworkbitxx@gmail.com>
2021-02-08 20:12:14 +0900

    Welcome to Java

---
+++ FirstProgram.java
@@ -0,0 +1,15 @@
+public class FirstProgram  
+{
+	public static void main(String[] args)	
+	{
+		System.out.println("Welcome to Java");
+		System.out.println("First Java Program");
+	}	
+}
+
+/*
+[실행 결과]
+Welcome to Java
+First Java Program
+계속하려면 아무 키나 누르십시오 . . .
+*/
